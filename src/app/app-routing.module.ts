a04bc1a2470d08a0a2710682e673e67e5524d0fb
Eisten Flores Sanchez <eisten.flores@unmsm.edu.pe>
2021-03-25 19:22:17 -0500

    creacion del ruteo

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,19 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeModule } from './pages/home/home.module';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    loadChildren: () =>
+      import('./pages/home/home.module').then((m) => m.HomeModule),
+  },
+  {
+    path: 'agent',
+    loadChildren: () =>
+      import('./pages/agent/agent.module').then((m) => m.AgentModule),
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
