77dd385c64b7839c8f40d35c7db005cd6d97e6fe
ronnymt <68928758+ronnymt@users.noreply.github.com>
2021-03-26 20:46:19 -0500

    version 2.0

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModuleModule } from './material-module/material-module.module';
+
 import { AppRoutingModule } from './app-routing.module';
+import { AgentModule } from './pages/agent/agent.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, BrowserAnimationsModule, MaterialModuleModule, AppRoutingModule],
+  imports: [BrowserModule, BrowserAnimationsModule, MaterialModuleModule, AppRoutingModule,AgentModule],
   providers: [],
   bootstrap: [AppComponent],
 })
--- src/app/pages/agent/agent.module.ts
+++ src/app/pages/agent/agent.module.ts
@@ -3,10 +3,11 @@ import { CommonModule } from '@angular/common';
 import { AgentComponent } from './agent.component';
 import { AgentRoutingModule } from './agent-routing.module';
 import { MaterialModuleModule } from 'src/app/material-module/material-module.module';
+import { ProductsAgentComponent } from './products-agent/products-agent.component';
 
 
 @NgModule({
-  declarations: [AgentComponent],
+  declarations: [AgentComponent,ProductsAgentComponent],
   imports: [CommonModule, AgentRoutingModule,MaterialModuleModule],
 })
 export class AgentModule {}
