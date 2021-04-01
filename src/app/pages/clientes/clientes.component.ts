7345810f299e82284b0afd5724d952b6c0d020b4
albert4554 <79956515+albert4554@users.noreply.github.com>
2021-03-31 19:25:20 -0500

    nuevo cambio

--- src/app/pages/clientes/clientes.component.ts
+++ src/app/pages/clientes/clientes.component.ts
@@ -1,19 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { CartService } from 'src/app/services/cart.service';
 import { ProductosService } from 'src/app/services/productos.service';
-
+import { map } from 'rxjs/operators';
+import { producto } from 'src/app/models/producto.models';
 @Component({
   selector: 'app-clientes',
   templateUrl: './clientes.component.html',
   styleUrls: ['./clientes.component.css']
 })
 export class ClientesComponent implements OnInit {
-   
-  constructor(public productosService:ProductosService) { }
+   total$:Observable<number>
+   @Input() product: producto;
+  constructor(public productosService:ProductosService,
+    private cartService: CartService) { 
+      this.total$=this.cartService.cart$.pipe(map(products=>products.length));
+    }
 
   ngOnInit(): void {
   }
   obtenerProductos(){
     return this.productosService.getProducts();
   }
-
+  addCart() {
+    console.log('añadir al carrito');
+    this.cartService.addCart(this.product);
+    // this.productClicked.emit(this.product.id);
+  }
 }
