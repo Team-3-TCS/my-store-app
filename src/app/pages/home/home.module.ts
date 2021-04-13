840b911dadfb80912d7966198f4039f433b4ffa9
Eisten Flores Sanchez <eisten.flores@unmsm.edu.pe>
2021-04-12 23:11:52 -0500

    corriguiendo los headers y footers

--- src/app/pages/home/home.module.ts
+++ src/app/pages/home/home.module.ts
@@ -3,9 +3,15 @@ import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home.component';
 import { HomeRoutingModule } from './home-routing.module';
 import { MaterialModuleModule } from 'src/app/material-module/material-module.module';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 @NgModule({
   declarations: [HomeComponent],
-  imports: [CommonModule, HomeRoutingModule,MaterialModuleModule],
+  imports: [
+    CommonModule,
+    HomeRoutingModule,
+    MaterialModuleModule,
+    SharedModule,
+  ],
 })
 export class HomeModule {}
--- src/app/pages/login/login.module.ts
+++ src/app/pages/login/login.module.ts
@@ -4,6 +4,7 @@ import { LoginComponent } from './login.component';
 import { LoginRoutingModule } from './login-routing.module';
 import { MaterialModuleModule } from 'src/app/material-module/material-module.module';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 @NgModule({
   declarations: [LoginComponent],
@@ -13,6 +14,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     MaterialModuleModule,
     FormsModule,
     ReactiveFormsModule,
+    SharedModule,
   ],
 })
 export class LoginModule {}
